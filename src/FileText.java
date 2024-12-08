fbc2c7700bf203fb481ddb5a47abab97ea7e4412
unknown <filson.mark777@gmail.com>
2024-12-08 13:49:16 +1000

    lessoin commit

---
+++ src/FileText.java
@@ -0,0 +1,43 @@
+package HomeWorkPractice;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class FileText {
+    public static void main(String[] args) {
+
+        File text = new File("C:\\Users\\User\\IdeaProjects\\lesson1\\src\\main\\resources\\text.txt");
+        try (PrintWriter printWriter = new PrintWriter(text)){
+            printWriter.println("Люблю грозу в начале мая,\n" +
+                    "Когда весенний, первый гром,\n" +
+                    "Как бы резвяся и играя,\n" +
+                    "Грохочет в небе голубом.\n" +
+                    "Гремят раскаты молодые,\n" +
+                    "Вот дождик брызнул, пыль летит,\n" +
+                    "Повисли перлы дождевые,\n" +
+                    "И солнце нити золотит.\n" +
+                    "С горы бежит поток проворный,\n" +
+                    "В лесу не молкнет птичий гам,\n" +
+                    "И гам лесной, и шум нагорный –\n" +
+                    "Все вторит весело громам.\n" +
+                    "Ты скажешь: ветреная Геба,\n" +
+                    "Кормя Зевесова орла,\n" +
+                    "Громокипящий кубок с неба,\n" +
+                    "Смеясь, на землю пролила.");
+        }catch (FileNotFoundException fileNotFoundException){
+            fileNotFoundException.printStackTrace();
+
+        }
+
+
+        File text2 = new File("C:\\Users\\User\\IdeaProjects\\lesson1\\src\\main\\resources\\text2.txt");
+        try (PrintWriter printWriter = new PrintWriter(text2)){
+            printWriter.println();
+        }catch (FileNotFoundException fileNotFoundException){
+            fileNotFoundException.printStackTrace();
+
+        }
+
+    }
+}
